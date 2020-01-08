8bb2f5cb7220bcee485c7143688152fcb8eb3ca3
KrzysztofNoga <krzysztofnoga1337@gmail.com>
2020-01-08 23:50:57 +0100

    Initial Commit: Create project.

---
+++ src/main/java/pl/noga/PortalOskApp/PortalOskApplication.java
@@ -0,0 +1,13 @@
+package pl.noga.PortalOskApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalOskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalOskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/noga/PortalOskApp/PortalOskApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.noga.PortalOskApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalOskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
